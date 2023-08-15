70b2cab8b6b00fe569914be65f2ae2ec139c02ce
= <=>
2023-08-15 11:57:25 -0700

    my frontend

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+const btn = document.querySelector('body');
+btn.addEventListener('click', function onClick(e)
+{
+    document.body.style.backgroundSize="cover"
+    document.body.style.backgroundImage="url('https://thumbs.gfycat.com/QuarterlyOptimalBedlingtonterrier-max-1mb.gif')";
+})
+btn.addEventListener('dblclick', function onClick(e)
+{
+    document.body.style.backgroundSize="cover"
+    document.body.style.backgroundImage="url('https://media.tenor.com/Miexp8zUcQoAAAAi/transparent-background-flowers-background.gif')";
+})
+})
